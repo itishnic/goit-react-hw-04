84d6c0b9b32c02bad643534883234a3044c4385b
itishnic <122455719+itishnic@users.noreply.github.com>
2024-06-27 12:12:39 +0300

    commit

---
+++ src/components/ImageModal/ImageModal.jsx
@@ -0,0 +1,29 @@
+import Modal from "react-modal";
+
+import css from "./ImageModal.module.css";
+
+Modal.setAppElement("#root");
+
+const ImageModal = ({ isOpen, onRequestClose, picture }) => {
+  return (
+    <Modal
+      isOpen={isOpen}
+      onRequestClose={onRequestClose}
+      shouldCloseOnOverlayClick={true}
+      shouldCloseOnEsc={true}
+      className={css.modal}
+    >
+      {picture && (
+        <div className={css.modalContent}>
+          <img
+            src={picture.urls.regular}
+            alt={picture.alt_description}
+            className={css.picture}
+          />
+        </div>
+      )}
+    </Modal>
+  );
+};
+
+export default ImageModal;
